4de177c23bf81fa16166bd513039fa5e7f95d696
seoHyun99 <seu1140@naver.com>
2017-01-04 15:25:23 +0900

    2017-01-04

---
+++ SowrenderNumber/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace SowrenderNumber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputNum = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.printNum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputNum
+            // 
+            this.inputNum.Location = new System.Drawing.Point(22, 37);
+            this.inputNum.Name = "inputNum";
+            this.inputNum.Size = new System.Drawing.Size(152, 21);
+            this.inputNum.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(192, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // printNum
+            // 
+            this.printNum.Location = new System.Drawing.Point(22, 87);
+            this.printNum.Name = "printNum";
+            this.printNum.Size = new System.Drawing.Size(245, 85);
+            this.printNum.TabIndex = 2;
+            this.printNum.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 198);
+            this.Controls.Add(this.printNum);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.inputNum);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputNum;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label printNum;
+    }
+}
+
---
+++ SowrenderNumber/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SowrenderNumber
+{
+    /*
+     * 0~999까지의 숫자를 입력했을 때, 입력받은 수를 한글로 출력해주는 프로그램.
+     */
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int inputNumFromUser;
+            if (!String.IsNullOrWhiteSpace(inputNum.Text) && int.TryParse(inputNum.Text, out inputNumFromUser))
+            {
+                inputNumFromUser = int.Parse(inputNum.Text);
+                if (inputNumFromUser > 999)
+                {
+                    MessageBox.Show("0~999 사이의 숫자를 입력해주세요.");
+                }
+                else
+                {
+                    int[] numberArray = new int[3];
+                    numberArray = returnArray(inputNumFromUser, returnNumSize(inputNumFromUser));
+
+                    printNum.Text = inputNumFromUser + " : " + returnString(numberArray);
+                }
+            }
+            else {
+                MessageBox.Show("숫자를 입력해주세요.");
+                printNum.Text = "";
+            }
+        }
+
+        private int[] returnArray(int number, int size)
+        {
+            int[] numberArray = new int[3];
+            if (size == 1) {
+                numberArray[0] = number; // 일의자리
+                numberArray[1] = 0;
+                numberArray[2] = 0;
+            }
+            else if (size == 2)
+            {
+                numberArray[0] = number % 10; // 일의자리
+                numberArray[1] = number / 10; // 십의자리
+                numberArray[2] = 0;
+            }
+            else
+            {
+                numberArray[0] = number % 10; // 일의자리
+                number -= number % 10;
+                numberArray[1] = (number % 100) / 10; // 십의자리
+                numberArray[2] = number / 100; // 백의자리
+            }
+            return numberArray;
+        }
+
+        private int returnNumSize(int num) // 입력받은 숫자의 크기 계산
+        {
+            int numSize = 0;
+
+            if (num > 99)
+                numSize = 3;
+
+            else if (num > 9)
+                numSize = 2;
+
+            else
+                numSize = 1;
+
+            return numSize;
+        }
+
+        private string returnString(int[] numArray) {
+            string result = null;
+
+            for (int index = 2; index >= 0; index--) {
+                if (numArray[index] != 0) {
+                    switch (numArray[index])
+                    {
+                        case 1:
+                            if (index == 0)
+                                result += "일"; break;
+                        case 2:
+                            result += "이"; break;
+                        case 3:
+                            result += "삼"; break;
+                        case 4:
+                            result += "사"; break;
+                        case 5:
+                            result += "오"; break;
+                        case 6:
+                            result += "육"; break;
+                        case 7:
+                            result += "칠"; break;
+                        case 8:
+                            result += "팔"; break;
+                        case 9:
+                            result += "구"; break;
+                    }
+                    switch(index){
+                        case 2:
+                            result += "백"; break;
+                        case 1:
+                            result += "십"; break;
+                    }
+                }
+            }
+            if (String.IsNullOrWhiteSpace(result)) {
+                result = "영";
+            }
+       
+            return result;
+        } 
+    }
+}
